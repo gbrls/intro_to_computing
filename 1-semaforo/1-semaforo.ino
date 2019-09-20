808df059f56869a6faa3132cdb5a17b9f13f6e3c
vituriano <viturianooliveira@gmail.com>
2019-09-20 10:47:32 -0400

    Comentarios bonitinhos

--- 1-semaforo/1-semaforo.ino
+++ 1-semaforo/1-semaforo.ino
@@ -2,8 +2,11 @@
 #define vermelho 12
 #define verde 13
 #define entrada_botao 2
+#define TIME_VERDE 6000
+#define TIME_AMARELO 1000
+#define TIME_VERMELHO 3000
 int n;
-int btn_press=0; /* variavel para guardar se o botão ja foi pressionado */
+int btn_press = 0; /* variavel para guardar se o botão ja foi pressionado */
 void setup()
 {
   pinMode(amarelo, OUTPUT);
@@ -13,28 +16,32 @@ void setup()
 }
 
 void loop()
-{
-  btn_press=0; /* reiniciar o estado do botão */
-  for (n = 0; n < 6000; n++)
+{                
+  // ACENDE O VERDE
+  btn_press = 0; /* reiniciar o estado do botão */
+  for (n = 0; n < TIME_VERDE; n++)
   {
     int botao = digitalRead(entrada_botao);
     if (botao == 1 && !btn_press)
     {
-      n += (6000 - n) / 2;
-      btn_press=1;
+      n += (TIME_VERDE - n) / 2;
+      btn_press = 1;
     }
     digitalWrite(verde, LOW);
     digitalWrite(vermelho, LOW);
     digitalWrite(verde, HIGH);
     delay(1);
   }
+
+  // ACENDE O AMARELO
   digitalWrite(verde, LOW);
   digitalWrite(amarelo, HIGH);
   digitalWrite(vermelho, LOW);
-  delay(1000);
+  delay(TIME_AMARELO);
+
+  // ACENDE O VERMELHO
   digitalWrite(verde, LOW);
   digitalWrite(amarelo, LOW);
   digitalWrite(vermelho, HIGH);
-  delay(3000);
-
+  delay(TIME_VERMELHO);
 }
