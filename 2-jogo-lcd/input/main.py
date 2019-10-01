140d82e9bb2db7c1b0c1e08de592b449614e02fa
gbrls <gabrielschneider100@protonmail.com>
2019-10-01 19:02:13 -0300

    Adicinada comunicação serial entre o Python e o Arduino

--- 2-jogo-lcd/input/main.py
+++ 2-jogo-lcd/input/main.py
@@ -1,11 +1,32 @@
-import os, pyxhook
+import os, pyxhook, serial
 
 # TODO: saida serial p/o arduino
 
+# Mapeamento de Teclas para comandos que serão enviados p/o Arduino
+key_dict = {"Left": -1, "Right": 1,
+        "a": -1, "d": 1,
+        "space": 0}
+
+# Nenhum desses valores valem nada. São só placeholders
+port = "/dev/algumacoisa"
+ard_serial = serial.Serial(port, 9600,timeout=2)
+
+# Transformar a String da tecla em um comando entendivel pelo Arduino
+def EncodeKey(key):
+    if key in key_dict:
+        return key_dict[key]
+
+def SendData(key):
+    encoded = EncodeKey(key)
+    if encoded != None:
+        print("Sending: {}".format(encoded))
+        # TODO: não sei se isso funciona
+        ard_serial.write(str(encoded))
 
 # Função que será chamada quando uma tecla for pressionada
 def OnKeyPress(event):
     print("Key: {}".format(event.Key))
+    SendData(event.Key)
 
 # Conectar o hook
 hook = pyxhook.HookManager()
